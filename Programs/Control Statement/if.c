91d64d7e2f08953524477458d1bbba9da4089d37
Suman Basnet <Arunbasnet54@gmail.com>
2021-10-24 14:51:29 +0545

    Use of if condition

---
+++ Programs/Control Statement/if.c
@@ -0,0 +1,16 @@
+// Use of if cindition//
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num;
+    printf("\t Enter a number : ");
+    scanf("%d", &num);
+
+    if(num>=0)
+    {
+        printf("\tThe number is positive number.");
+    }
+return 0;
+}
