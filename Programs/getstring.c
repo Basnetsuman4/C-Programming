f7ed0c7b97876dab3aadb42978618977b040a0e3
Suman Basnet <Arunbasnet54@gmail.com>
2021-10-24 14:30:51 +0545

    Use of gets and puts

---
+++ Programs/getstring.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char name[20], address[30];
+    printf("\tEnter your name : ");
+    gets(name);
+    printf("\n\tEnter your address : " );
+    gets(address);
+ 
+
+
+    printf("\n\tYour name is : ");
+    puts(name);
+
+    printf("\n\tYour address is : ");
+    
+    puts(address);
+    return 0;
+}
