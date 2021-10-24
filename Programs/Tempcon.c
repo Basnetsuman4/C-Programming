0217af34f4fee312b345104bf9a301270ecf4745
Suman Basnet <80586555+Basnetsuman4@users.noreply.github.com>
2021-10-24 09:57:52 +0545

    Temperature conversion

--- Programs/Tempcon.c
+++ Programs/Tempcon.c
@@ -7,7 +7,7 @@ int main()
     printf("\n\tEnter the temerature in Celcius : ");
     scanf("%f",&clecius);
 
-    fahern =( (9 * clecius)/5 )+32;
+    fahern =( (9 * clecius)/5 )+32 ;
     printf("\n\tThe temperature in Fahernheit is %f ", fahern);
     return 0;
 }
