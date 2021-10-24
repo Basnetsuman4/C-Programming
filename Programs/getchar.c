9fcf574bfe2df54950216154fe2a4daac591cb68
Suman Basnet <Arunbasnet54@gmail.com>
2021-10-24 14:16:13 +0545

    Use of getchar and putchar

---
+++ Programs/getchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char gender;
+    printf("Enter your gender in M for male or F for female : ");
+    gender = getchar();
+    printf("\n\tThe entered gender is  : ");
+    putchar (gender);
+}
