cc4fcfa51ddab27ec1f9f060dcff7396ded01af4
Suman Basnet <Arunbasnet54@gmail.com>
2021-10-25 14:15:02 +0545

    Use of while loop

---
+++ Programs/Loop/While.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0 ;
+    while (i<4)
+    {
+        printf("\n\tMEOW");
+        i++;  //increment necessary, if not then it executes in infinite loop//
+    }
+    
+return 0;
+}
