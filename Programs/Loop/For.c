a398453ba73fc4d767e32d18686504f6c456b1e3
Suman Basnet <Arunbasnet54@gmail.com>
2021-10-25 13:59:59 +0545

    Use of for loop in C

---
+++ Programs/Loop/For.c
@@ -0,0 +1,17 @@
+/*use of for loop
+used to execute several number of statement*/
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i,j;
+    for(i=0;i<2;i++) //counter initilization; test condition ; update counter//
+    {
+        for(j=0;j<i;j++)
+        {
+        printf("\n\t\nLet's goooo1111");
+        }
+    }
+return 0;
+}
